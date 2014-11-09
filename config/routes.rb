47f6ee7bdd83645eeebed87ea54ca83bb82522a7
fishermanswharff <fishermanswharff@github.com>
2014-11-08 19:00:49 -0500

    routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :line_items
   resources :carts
   resources :products
+  resources :users
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
